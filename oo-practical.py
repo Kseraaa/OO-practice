863df2b417d3a4a19fc590e5defb0ff3cca5ab38
ธนภัทร ด้วงแก้ว <thanapat0918618713@gmail.com>
2022-03-29 14:15:25 +0700

    cal_area

--- oo-practical.py
+++ oo-practical.py
@@ -1,7 +1,24 @@
+import math
+
+class Shape:
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def cal_area(self):
+        pass
 class Rectangle:
     def __init__(self, wide, long):
         self.wide = wide
         self.long = long
+
+    def cal_area(self):
+        return self.wide * self.long
+
 class Circle:
     def __init__(self, radius):
         self.radius = radius
+
+    def cal_area(self):
+        return math.pi * self.radius * self.radius
+
