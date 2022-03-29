6cbb9a1084210ab0f723f6ae0261443a94ef19ea
ธนภัทร ด้วงแก้ว <thanapat0918618713@gmail.com>
2022-03-29 14:39:40 +0700

    finished

--- oo-practical.py
+++ oo-practical.py
@@ -1,24 +1,45 @@
 import math
+from abc import ABC, abstractmethod
 
 class Shape:
-    def __init__(self, name):
+    def __init__(self,name) :
         self.name = name
-
-    @abstractmethod
-    def cal_area(self):
+    
+    def cal_area(self) :
         pass
-class Rectangle:
-    def __init__(self, wide, long):
-        self.wide = wide
-        self.long = long
 
-    def cal_area(self):
-        return self.wide * self.long
+class Rectangle(Shape) :
+    def __init__(self,name,width,length) :
+        super().__init__(name)
+        self.width = width
+        self.length = length
+        
+    def cal_area(self) :
+        return (self.width)*(self.length)
 
-class Circle:
-    def __init__(self, radius):
+class Circle(Shape) :
+    def __init__(self, name,radius):
+        super().__init__(name)
         self.radius = radius
-
     def cal_area(self):
-        return math.pi * self.radius * self.radius
+        return (math.pi)*((self.radius)**2)
+
+def cal_total_area(shapes):
+    total = 0
+    for shape in shapes:
+        total += shape.cal_area()
+    return total
+
+r1 = Rectangle('r1',4,5)
+r2 = Rectangle('r2',10,12)
+c1 = Circle('c1',10)
+print(r1.cal_area())
+print(r2.cal_area())
+print(c1.cal_area())
+shapes = [r1,r2,c1]
+total_area = cal_total_area(shapes)
+print(total_area)
+
+
+
 
